d5d41d01a40a6f8020665e6bd095f6837f98ce40
Isaac Smith <isaac.smitty@gmail.com>
2019-03-22 18:03:28 +0000

    added basic auth to admin/categories and admin/products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+http_basic_authenticate_with name: ENV['admin_name'], password: ENV['admin_password']
+
   def index
     @products = Product.order(id: :desc).all
   end
