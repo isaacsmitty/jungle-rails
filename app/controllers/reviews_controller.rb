a261e9a8973e32cfb5c3d3802ec4ca02a8e0eae5
Isaac Smith <isaac.smitty@gmail.com>
2019-03-25 13:42:52 +0000

    added rspec to project

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,8 @@
 class ReviewsController < ApplicationController
 
-  def show
-    @product = Product.find params[:id]
-    @review = Review.all
-  end
+  # def show
+  #   @product = Product.find params[:id]
+  #   @review = Review.where(product_id: @product.id)
+  # end
 
 end
